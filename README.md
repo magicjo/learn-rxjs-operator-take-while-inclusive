044839239d1ae84d23fcdf52ffe30f679e5479a9
Johan GIRARD <magicjo@tiglab.net>
2019-06-24 22:10:59 +0200

    Improve example

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+[![Build Status](https://travis-ci.org/magicjo/learn-rxjs-operator-take-while-inclusive.svg?branch=master)](https://travis-ci.org/magicjo/learn-rxjs-operator-take-while-inclusive)
+
 # learn-rxjs-operator-take-while-inclusive
 
+Example about the filtering operator `takeWhile` with the `inclusive` flag.
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/learn-rxjs-operator-take-while-inclusive)
+
+## How to execute in local ?
+
+Execute `src/index.ts`:
+```bash
+docker run -v $(pwd):/app/src magicjo/learn-rxjs:latest start
+```
+
+Execute `tslint`:
+```bash
+docker run -v $(pwd):/app/src magicjo/learn-rxjs:latest lint
+```
--- index.ts
+++ index.ts
@@ -1,9 +1,31 @@
-import { of } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+// RxJS v6+
+import { of } from "rxjs";
+import { takeWhile } from "rxjs/operators";
 
+/*
+ Filtering Operator: takeWhile
+ -----------------------------
 
-const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
-);
+ Takes values from the source only while they pass the condition given.
+ When the first value does not satisfy, it completes.
+ ```
+ takeWhile(predicate: function(value, index): boolean, inclusive?: boolean): Observable
+ ```
 
-source.subscribe(x => console.log(x));
+ http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-takeWhile
+*/
+
+// Emit: 0, 1, 2, 4, 8, 9, 10
+const source = of(0, 1, 2, 4, 8, 9, 10);
+
+// Allow values until value from source is greater than 5, then complete
+// with inclusive flag, the value causing the predicate to return false will also be emitted
+const example = source.pipe(takeWhile(n => n <= 5, true));
+
+// Receive: 0, 1, 2, 4, 8
+console.log("[start]");
+example.subscribe({
+  complete: () => console.log("[complete]"),
+  error: err => console.error("[error] : ", err),
+  next: value => console.log("[next] : ", value)
+});
